8efbff7d4a434fc41438a3585c02c4ec5eb4dcc4
Keith Hanson <hanson2@rose-hulman.edu>
2018-03-16 16:15:48 -0400

    playfair for quiz

--- src/basic/Substitution.java
+++ src/basic/Substitution.java
@@ -55,32 +55,144 @@ public class Substitution {
 				}
 				
 			}else if(temp.toLowerCase().charAt(0)=='t'){
-				System.out.println(tempFunction(0));
-				System.out.println(tempFunction(1));
-				System.out.println(tempFunction(2));
-				System.out.println(tempFunction(-1));
-				System.out.println(tempFunction(46));
-				System.out.println(tempFunction(47));
+				System.out.println("Enter phrase to decode:");
+				temp = scan.nextLine();
+				tempFunction(temp, scan);
 			}
 		}
 		}
-	private static int tempFunction(int fib) {
+	private static void tempFunction(String input, Scanner scan) {
 		// TODO broken
-		int ans;
-		if (fib<0) {
-			return -100;
-		}else if(fib==0){
-			return 0;
-		}else if(fib == 1){
-			return 1;
-		}else{
-			ans=tempFunction(fib-1)+tempFunction(fib-2);
-			if (ans<0){
-				return -100;
-			}else{
-				return ans;
+		String temp = "";
+		String ans = "";
+		System.out.println("Please enter the password:");
+		temp = scan.nextLine();
+		char[][] key = new char[5][5];
+		ArrayList<Character> inputs = new ArrayList<Character>();
+		for (int i = 0; i < key.length; i++) {
+			for (int j = 0; j < key.length; j++) {
+				if ((i+j)<temp.length()) {
+					if (inputs.contains(temp.charAt(i+j))) {
+						if (temp.charAt(i) == 'j') {
+							key[i][j] = 'i';
+							inputs.add('i');
+							inputs.add('j');
+						}
+						else{
+							key[i][j] = temp.charAt(i+j);
+						}
+					}
+				}else{
+					if (!inputs.contains('a')) {
+						inputs.add('a');
+						key[i][j]='a';
+					}else if (!inputs.contains('b')) {
+						inputs.add('b');
+						key[i][j]='b';
+					}else if (!inputs.contains('c')) {
+						inputs.add('c');
+						key[i][j]='c';
+					}else if (!inputs.contains('d')) {
+						inputs.add('d');
+						key[i][j]='d';
+					}else if (!inputs.contains('e')) {
+						inputs.add('e');
+						key[i][j]='e';
+					}else if (!inputs.contains('f')) {
+						inputs.add('f');
+						key[i][j]='f';
+					}else if (!inputs.contains('g')) {
+						inputs.add('g');
+						key[i][j]='g';
+					}else if (!inputs.contains('h')) {
+						inputs.add('h');
+						key[i][j]='h';
+					}else if (!inputs.contains('i')) {
+						inputs.add('i');
+						key[i][j]='i';
+					}else if (!inputs.contains('k')) {
+						inputs.add('k');
+						key[i][j]='k';
+					}else if (!inputs.contains('l')) {
+						inputs.add('l');
+						key[i][j]='l';
+					}else if (!inputs.contains('m')) {
+						inputs.add('m');
+						key[i][j]='m';
+					}else if (!inputs.contains('n')) {
+						inputs.add('n');
+						key[i][j]='n';
+					}else if (!inputs.contains('o')) {
+						inputs.add('o');
+						key[i][j]='o';
+					}else if (!inputs.contains('p')) {
+						inputs.add('p');
+						key[i][j]='p';
+					}else if (!inputs.contains('q')) {
+						inputs.add('q');
+						key[i][j]='q';
+					}else if (!inputs.contains('r')) {
+						inputs.add('r');
+						key[i][j]='r';
+					}else if (!inputs.contains('s')) {
+						inputs.add('s');
+						key[i][j]='s';
+					}else if (!inputs.contains('t')) {
+						inputs.add('t');
+						key[i][j]='t';
+					}else if (!inputs.contains('u')) {
+						inputs.add('u');
+						key[i][j]='u';
+					}else if (!inputs.contains('v')) {
+						inputs.add('v');
+						key[i][j]='v';
+					}else if (!inputs.contains('w')) {
+						inputs.add('w');
+						key[i][j]='w';
+					}else if (!inputs.contains('x')) {
+						inputs.add('x');
+						key[i][j]='x';
+					}else if (!inputs.contains('y')) {
+						inputs.add('y');
+						key[i][j]='y';
+					}else if (!inputs.contains('z')) {
+						inputs.add('z');
+						key[i][j]='z';
+					}
+				}
 			}
 		}
+		for (int k = 0; k < temp.length(); k++) {
+			int[] loc1 = findLocation(key, temp.charAt(k));
+			int[] loc2 = findLocation(key, temp.charAt(k+1));
+			if (loc1.equals(loc2)) {//same thing twice
+				ans+=key[loc1[0]][loc1[1]]+key[loc1[0]][loc1[1]];
+			}else if(loc1[0]==loc2[0]){//same colomn
+				ans+=key[loc1[0]][(loc1[1]-1)%5]+key[loc2[0]][(loc2[1]-1)%5];
+			}else if(loc1[1]==loc2[1]){//same row
+				ans+=key[(loc1[0]-1)%5][loc1[1]]+key[(loc2[0]-1)%5][loc2[1]];
+			}else{//different row and column
+				ans+=key[loc1[0]][loc2[1]]+key[loc2[0]][loc1[1]];
+			}
+			k++;//go by 2 each cycle
+		}
+		System.out.println(ans);
+		
+	}
+	
+	@SuppressWarnings("unused")
+	private static int[] findLocation(char[][] key, char letter){
+		int[] ans = new int[2];
+		for (int i = 0; i < key.length; i++) {
+			for (int j = 0; j < key.length; j++) {
+				if (letter == key[i][j]){
+					ans[0] = i;
+					ans[1] = j;
+					return ans;
+				}
+			}
+		}
+		return ans;
 	}
 		
 	@SuppressWarnings("unchecked")
